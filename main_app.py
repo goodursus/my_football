add2296981e3e4f37ceb5d7a9d30e175398cdb3e
goodursus <goodursus@gmail.com>
2024-10-16 22:24:42 +1100

    New Host

--- main_app.py
+++ main_app.py
@@ -1249,4 +1249,5 @@ def display_modal(selected_value):
     return False, "", ""
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+#    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0', port=10000)
